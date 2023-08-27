366414bc81ca5dda71a966404344807d6476f18b
unknown <rafaelcutrimalmeida@gmail.com>
2023-08-27 14:36:16 -0300

    Final

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_prime(int num) {
-    if (num <= 1) {
-        return 0; 
-    }
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return 0;  
+verifica_numero_primo(int n){
+int i;
+if(n<=1){
+    printf("0");
+}else{
+    for(i=2; i< n; i++){
+        if(n%i==0){
+            printf("0");
+            break;
         }
     }
-
-    return 1;  
+    if(i==n){
+        printf("1");
+    }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: %s <numero>\n", argv[0]);
-        return 1;
-    }
-
-    int numero = atoi(argv[1]);
-
-    if (is_prime(numero)) {
-        printf("1 - O numero %d eh primo.\n", numero);
-    } else {
-        printf("0 - O numero %d nao eh primo.\n", numero);
-    }
-
-    return 0;
 }
+int main(){
+    int n;
+    printf("Digite um numero\n");
+    scanf("%d",&n);
+    verifica_numero_primo(n);
+}
+
+
+
+
+
+
+
+
+
+
+
